a98758972c2b33bdbec678c26cacdce8885b224b
Psutinboy <grantledbetter12@gmail.com>
2024-10-24 12:46:53 -0500

    10-24-2024

--- Dice_challenge/index.js
+++ Dice_challenge/index.js
@@ -1,34 +1,23 @@
-// JavaScript code will go here
-console.log("index.js loaded successfully");
+var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-function rollDice() {
-  return Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
-
-function updateDiceImage(player, diceValue) {
-  const imgElement = document.querySelector(`.img${player}`);
-  imgElement.setAttribute('src', `images/dice${diceValue}.png`);
-}
-
-function determineWinner(player1Value, player2Value) {
-  const h1Element = document.querySelector('h1');
-  if (player1Value > player2Value) {
-    h1Element.textContent = 'Player 1 Wins!';
-  } else if (player2Value > player1Value) {
-    h1Element.textContent = 'Player 2 Wins!';
-  } else {
-    h1Element.textContent = 'Draw!';
-  }
-}
-
-function playGame() {
-  const player1Value = rollDice();
-  const player2Value = rollDice();
-
-  updateDiceImage(1, player1Value);
-  updateDiceImage(2, player2Value);
-
-  determineWinner(player1Value, player2Value);
-}
-
-document.addEventListener('DOMContentLoaded', playGame);
